cf20f7cc40404a81fd803770d3a957f915468160
codereader <greebo@angua.at>
2025-11-14 15:41:26 +0100

    Level 3 complete

--- CCC41/Program.cs
+++ CCC41/Program.cs
@@ -1,6 +1,6 @@
 ﻿using CCC41Lib;
 
-var level = 2;
+var level = 3;
 
 WriteOutputs(level);
 
--- CCC41Lib/Solver.cs
+++ CCC41Lib/Solver.cs
@@ -42,13 +42,6 @@ public class Solver
         var actualLines = lines.Skip(1).ToList();
         var fullResult = new StringBuilder();
 
-        /*cases:
-
-        +: position++; time+=value X
-        0: time++
-        -: position--; time+=value
-
-        */
         foreach (var line in actualLines)
         {
             var parts = line.Split(' ');
@@ -68,13 +61,64 @@ public class Solver
         var actualLines = lines.Skip(1).ToList();
         var fullResult = new StringBuilder();
 
-        for (int i = 0; i < actualLines.Count; i++)
+        foreach (var line in actualLines)
         {
-            string? line = actualLines[i];
             var parts = line.Split(' ');
+
+            var stationPos = int.Parse(parts[0]);
+            var timeLimit = int.Parse(parts[1]);
+
+            var sequence = new List<int>();
+            sequence = CalculateSequence(stationPos, timeLimit);
+
+            var resultLine = string.Join(' ', sequence.Select(i => i.ToString()));
+            fullResult.AppendLine(resultLine);
         }
 
-        return fullResult.ToString().TrimEnd('\n').TrimEnd('\r');
+        return fullResult.ToString() + Environment.NewLine;
+    }
+    private List<int> CalculateSequence(int stationPos, int timeLimit)
+    {
+        var result = new List<int> { 0 };
+
+        if (Math.Abs(stationPos) >= 9)
+        {
+            result.AddRange( [ 5, 4, 3, 2, 1 ]); // accel
+
+            int additionalOnes = Math.Abs(stationPos) - 9;
+
+            for (int i = 0; i < additionalOnes; i++)
+            {
+                result.Add(1);
+            }
+
+            result.AddRange( [ 2, 3, 4, 5 ]); // decel
+        }
+        else // pos < 9
+        {
+            var steps = new List<int> { 5, 4, 3, 2, 1, 2, 3, 4, 5 };
+            var toRemove = 9 - Math.Abs(stationPos);
+
+            for (int i = 0; i < toRemove; i++)
+            {
+                var currentMin = steps.Min();
+                steps.RemoveAt(steps.IndexOf(currentMin));
+            }
+
+            result.AddRange(steps);
+        }
+
+        if (stationPos < 0)
+        {
+            for (int i = 0; i < result.Count; i++)
+            {
+                result[i] *= -1;
+            }
+        }
+
+        result.Add(0);
+
+        return result;
     }
 
     private string SolveLevel4(List<string> lines)
